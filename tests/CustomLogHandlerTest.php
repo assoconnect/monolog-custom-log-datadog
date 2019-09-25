2c46d19b417e558bd97ca377a6b504da3d0441c6
Mathieu Bourlet <mathieu.bourlet@assoconnect.com>
2019-09-25 14:16:43 +0200

    fixing the tests to make it work in CI

--- tests/CustomLogHandlerTest.php
+++ tests/CustomLogHandlerTest.php
@@ -3,37 +3,44 @@
 namespace AssoConnect\Tests;
 
 use AssoConnect\MonologDatadog\CustomLogHandler;
+use GuzzleHttp\ClientInterface;
 use Monolog\Logger;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
 
-class ClientTest extends TestCase
+final class CustomLogHandlerTest extends TestCase
 {
-    protected function createCustomLogHandler(): CustomLogHandler
+    private const ENDPOINT = 'foo';
+    private const API_KEY = 'bar';
+
+    private $clientMock;
+
+    private $customLogHandler;
+
+    protected function setUp()
     {
-        $endpoint = getenv('DATADOG_ENDPOINT');
-        $apiKey = getenv('DATADOG_API_KEY');
-
-        $guzzleClient = new \GuzzleHttp\Client();
-
-        $client = new CustomLogHandler($endpoint, $apiKey, $guzzleClient);
-
-        return $client;
+        $this->clientMock = $this->getMockBuilder(ClientInterface::class)->getMock();
+        $this->customLogHandler = new CustomLogHandler(self::ENDPOINT, self::API_KEY, $this->clientMock);
     }
 
-    public function testSuccess() :void
+    public function testItCallTheEndpointWithExpectedMessageAndHeaders()
     {
-        $customLogHandler = $this->createCustomLogHandler();
+        $this->clientMock->expects($this->once())->method('request')->with(
+            'POST',
+            self::ENDPOINT . '/v1/input/' . self::API_KEY,
+            $this->callback(function ($options) {
+                $body = json_decode($options['body'], true);
+
+                return $body['message'] === 'test'
+                    && $options['headers']['Content-Type'] == 'application/json'
+                    && $options['headers']['Accept'] == '*/*'
+                ;
+            })
+        )->willReturn($this->getMockBuilder(ResponseInterface::class)->getMock());
+
         $logger = new Logger('phpunit');
+        $logger->pushHandler($this->customLogHandler);
 
-        $logger->pushHandler($customLogHandler);
-
-        $logger->pushProcessor(function ($record) {
-            $record['hostname'] = 'phpunit';
-            $record['ddsource'] = 'php';
-            $record['service'] = 'phpunit';
-            return $record;
-        });
-
-        $this->assertTrue($logger->info('test'));
+        $logger->info('test');
     }
 }
